3c979bf35fd8c58de394161c4828d2d76b16eff2
yrizos <yrizos@gmail.com>
2014-12-18 06:04:44 +0200

    first commit

--- tests/autoload.php
+++ tests/autoload.php
@@ -1,4 +1,5 @@
 <?php
 
-$autoloader = include_once __DIR__ . '/../vendor/autoload.php';
+$loader = include_once __DIR__ . '/../vendor/autoload.php';
+$loader->addPsr4("DataEntityTest\\", __DIR__ . '/src');
  
